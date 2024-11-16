8d6b411cf20003ffbc60ea6cf67e73625afc63c2
papertots <12170984+papertots@users.noreply.github.com>
2024-11-15 22:08:34 -0500

    added in-memory recommendation DAO, and a run configuration where all api calls are replaced by dummy api calls

---
+++ src/main/java/app/MainInMemoryOnly.java
@@ -0,0 +1,36 @@
+package app;
+
+import data_access.InMemoryRecommendationDAO;
+import data_access.InMemoryUserDAO;
+import data_access.UserRepository;
+import use_case.generate_recommendations.GenDataAccessInterface;
+
+/**
+ * A media recommendation application, but with dummy api calls.
+ */
+public class MainInMemoryOnly {
+
+    /**
+     * The entrypoint of the application where all external api calls are replaced by dummy implementations.
+     */
+    public static void main(String[] args) {
+        final UserRepository inMemoryUserDAO = new InMemoryUserDAO();
+        final GenDataAccessInterface inMemoryRecommendationDAO = new InMemoryRecommendationDAO();
+
+        final AppBuilder builder = new AppBuilder();
+        builder.addUserDAO(inMemoryUserDAO)
+                .addGenDAO(inMemoryRecommendationDAO)
+                .addBlankView()
+                .addNoteView()
+                .addLoginView()
+                .addSignupView()
+                .addLoggedInView()
+                .addNoteUseCase()
+                .addGenUseCase()
+                .addSignupUseCase()
+                .addLoginUseCase()
+                .addChangePasswordUseCase()
+                .addLogoutUseCase()
+                .build().setVisible(true);
+    }
+}
